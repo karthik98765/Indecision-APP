7e4bb44cd6d723ae66eda5d5afb6bd2defd45def
karthi <karthi@exm.com>
2021-02-18 13:12:57 +0530

    default export

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+
+export const add = (a, b) => a + b;
+
+export default (a, b) => a - b;
+
+// export { square, add, sub as default };
+
+// export { square, add };
