abe69143cbd9e87b89c3878fc7baa9e15ce53117
karthi <karthi@exm.com>
2021-02-20 16:32:00 +0530

    one Component file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-// import './utils.js';
-// import sub, { square, add } from './utils';
-
-// console.log('app is running');
-// console.log(square(4));
-// console.log(add(4, 5));
-// console.log(sub(100, 81));
-
-import isSenior, { isAdult, canDrink } from './person';
-
-console.log(isAdult(17));
-console.log(canDrink(23));
-console.log(isSenior(65));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/Indecision';
+  
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
